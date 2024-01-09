4557aa0e9720b5e05fa3c1154f40b007fb1beaf4
Sonya Nadtochey <nadtocheysonya@gmail.com>
2024-01-09 21:43:47 +0300

    api3

--- script.js
+++ script.js
@@ -4,16 +4,48 @@ const mainUrl = 'http://exam-2023-1-api.std-900.ist.mospolytech.ru';
 
 const routesUrl = '/api/routes';
 
-function getData() {
-    const xhr = new XMLHttpRequest();
-    const url = new URL(routesUrl, mainUrl);
-    url.searchParams.set('api_key', apiKey);
-    xhr.open('get', url);
-    xhr.send();
-    xhr.onload = function() {
-        console.log(xhr.response);
-    }
+function selectRoute(event) {
+    //доделать обработчик
 }
+
+function addTableRow(record) {
+    const tr = document.createElement('tr');
+    tr.id = record.id;
+    const name = document.createElement('td');
+    name.textContent = record.name;
+    tr.append(name);
+    const description = document.createElement('td');
+    description.textContent = record.description;
+    tr.append(description);
+    const mainObject = document.createElement('td');
+    mainObject.textContent = record.mainObject;
+    tr.append(mainObject);
+    const tdBtn = document.createElement('td');
+    tdBtn.textContent = record.tdBtn;
+    tdBtn.addEventListener('click', selectRoute)
+    tr.append(tdBtn);
+} 
+
+// function getData() {
+//     const xhr = new XMLHttpRequest();
+//     const url = new URL(routesUrl, mainUrl);
+//     url.searchParams.set('api_key', apiKey);
+//     xhr.open('get', url);
+//     xhr.send();
+//     xhr.onload = function() {
+//         const results = JSON.parse(xhr.response);
+//         const tbody = document.querySelector('.tbody');
+//         for (const result of results) {
+//             console.log(result);
+//             tbody.innerHTML += `<tr id=${result.id}>
+//             <td>${result.name}</td>
+//             <td>${result.description}</td>
+//             <td>${result.mainObject}</td>
+//             <td><button>Выбрать</button></td>
+//             </tr>`
+//         }
+//     }
+// }
 
 window.addEventListener('DOMContentLoaded', ()=>{
     getData();
